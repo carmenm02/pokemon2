89e43c1b7e2bd9c185272475e584b051e8656d84
Carmen <91721886+carmenm02@users.noreply.github.com>
2022-04-28 07:58:38 +0000

    he creado la función para calcular la mediana

--- pokemon2.py
+++ pokemon2.py
@@ -16,5 +16,27 @@ def Media(caracteristica):
     for valor in caracteristica:
       Suma = Suma + valor
       mediaritmetica = Suma/n
-return Media
+  return Media
 
+#creo otra para la mediana
+
+def Mediana(caracteristica):
+  mediana = 0
+  caracteristica = caracteristica.sort_values()
+  caracteristica = caracteristica.reset_index(drop = True)
+  n = self.caracteristica.count()
+  par = False;
+  if (n%2 == 0):
+    print("El numero de observaciones es par")
+    par = True
+    if par:
+      rango = (n/2);
+      rangoPython = rango - 1
+      valor1 = caracteristica[rangoPython]
+      valor2 = caracteristica[rangoPython + 1]
+      mediana = valor1 + ((valor2-valor1)/2)
+    else:
+      rango = ((n + 1)/2)
+      rangoPython = rango - 1
+      mediana = caracteristica [rangoPython]
+    return Mediana
